14fc43df6f3570bdee258fd0a0bae542b6684273
Prakash Raje <prakash.solace123@gmail.com>
2025-07-22 21:54:15 +0530

    changed db name

--- config/database.php
+++ config/database.php
@@ -7,9 +7,9 @@
 
 // Production Use Database configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'u141519101_DEMO_SITE1');
+define('DB_USER', 'u141519101_everythingb2c1');
 define('DB_PASS', 'EveryThing@b2c#2025');
-define('DB_NAME', 'u141519101_DEMO_SITE1');
+define('DB_NAME', 'u141519101_everythingb2c1');
 
 // Create connection
 try {
