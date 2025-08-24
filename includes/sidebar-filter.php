eaf554d4472e69fcd688c068212477067e79fac9
Prakash Raje <prakash.solace123@gmail.com>
2025-08-24 23:49:06 +0530

    added latest header changes

--- includes/sidebar-filter.php
+++ includes/sidebar-filter.php
@@ -91,8 +91,8 @@ $categoryTree = buildCategoryTree($categories);
         </div>
       </div>
 
-      <!-- Price Range Filter - HIDDEN -->
-      <div class="filter-section" style="display: none;">
+      <!-- Price Range Filter -->
+      <div class="filter-section">
         <h5>Price Range</h5>
         <div class="price-range-container">
           <div class="price-display">
@@ -134,8 +134,8 @@ $categoryTree = buildCategoryTree($categories);
         </div>
       </div>
 
-      <!-- Sort Options - HIDDEN -->
-      <div class="filter-section" style="display: none;">
+      <!-- Sort Options -->
+      <div class="filter-section">
         <h5>Sort By</h5>
         <div class="form-group">
           <select name="sort" class="form-control">
