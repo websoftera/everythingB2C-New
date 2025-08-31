296647a249a2193cc2c80b0f1da4a5d6a0cb9e90
Prakash Raje <prakash.solace123@gmail.com>
2025-08-31 11:39:40 +0530

    added latest changes

--- config/database.php
+++ config/database.php
@@ -6,10 +6,10 @@
 // define('DB_NAME', 'everythingb2c');
 
 // Production Use Database configuration
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'u141519101_everythingb2c1');
-// define('DB_PASS', 'EveryThing@b2c#2025');
-// define('DB_NAME', 'u141519101_everythingb2c1');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u141519101_everythingb2c1');
+define('DB_PASS', 'EveryThing@b2c#2025');
+define('DB_NAME', 'u141519101_everythingb2c1');
 // Create connection
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
