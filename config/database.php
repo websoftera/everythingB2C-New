64c00a78bc3ccd9e10de67b25da3ce25caab1fae
Prakash Raje <prakash.solace123@gmail.com>
2025-08-31 11:39:26 +0530

    added latest changes

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Local Use Database configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'everythingb2c');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'everythingb2c');
 
 // Production Use Database configuration
 // define('DB_HOST', 'localhost');
