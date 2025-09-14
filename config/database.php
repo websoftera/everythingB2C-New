01a54c4bf104b046f1f4d9d876703f395e78c483
Prakash Raje <prakash.solace123@gmail.com>
2025-09-14 13:03:59 +0530

    added latest tracking system changes

--- config/database.php
+++ config/database.php
@@ -1,15 +1,15 @@
 <?php
 // Local Use Database configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'everythingb2c');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'everythingb2c');
 
 // Production Use Database configuration
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'u141519101_everythingb2c1');
-// define('DB_PASS', 'EveryThing@b2c#2025');
-// define('DB_NAME', 'u141519101_everythingb2c1');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u141519101_everythingb2c1');
+define('DB_PASS', 'EveryThing@b2c#2025');
+define('DB_NAME', 'u141519101_everythingb2c1');
 // Create connection
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
